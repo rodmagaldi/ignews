b53fc39bb38e1a3bde871349ec79c3f36d17bc34
Rodrigo Magaldi <rod.magaldi@gmail.com>
2021-08-23 00:29:36 -0300

    Finish ignite classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,26 @@
 import Image from "next/image";
 import logoImg from "../../../public/images/logo.svg";
 
+import { ActiveLink } from "../ActiveLink";
+
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SignInButton";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logoImg} alt="ig.news" />
         <nav>
-          <a className={styles.active} href="#">
-            Home
-          </a>
-          <a href="#">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
